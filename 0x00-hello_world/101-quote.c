5437e2e94e9bf195b11806832e8db82ad26ff32e
OmariYeboah <omariyeboah@gmail.com>
2023-02-16 21:14:54 -0800

    Task 4-8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry
- * Return: 1
+ * main - The c program that prints a line to a standard error
+ * Return: 1 (success)
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful/" " - Dora Korpar, 2015-10-19\n", 59);
+	fput(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
