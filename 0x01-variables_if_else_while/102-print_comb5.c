a7e02fee1338c6007799e5591ec56260586718f5
OmariYeboah <omariyeboah@gmail.com>
2023-02-20 02:28:48 -0800

    task 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,7 +15,7 @@ int main(void)
 			if (i < j)
 			{
 				putchar((i / 10) + 48);
-				putchar((i % 100) + 48);
+				putchar((i % 10) + 48);
 				putchar('');
 				putchar(j / 10) + 48);
 				putchar((j % 10) + 48);
